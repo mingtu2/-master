12b59276fccbfc7ef53390ba856f5deb810dd346
mingtu2 <1151320648@qq.com>
2022-04-07 22:58:22 +0800

    提交git测试

---
+++ src/com/zyp/Demo.java
@@ -0,0 +1,11 @@
+package com.zyp;
+
+/**
+ * @author mingtu
+ * @create 2022-04-07 22:50
+ */
+public class Demo {
+    public static void main(String [] args){
+        System.out.println("git test");
+    }
+}
---
+++ src/com/zyp/Demo2.java
@@ -0,0 +1,12 @@
+package com.zyp;
+
+/**
+ * @author mingtu
+ * @create 2022-04-07 22:50
+ */
+public class Demo2 {
+
+    public void show(){
+        System.out.println("show方法");
+    }
+}
